091043071d10e4af108d718c2dfc1b8b5d5c234b
aghiwardani <48787322+aghiwardani@users.noreply.github.com>
2020-08-29 01:41:53 +0700

    tugas hari 5

---
+++ resources/views/index_day4.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+      <h4>Tugas Hari 4</h4>
+       <ol>
+        <li><a href="{{ url('/string') }}">String</a></li>
+        <li><a href="{{ url('/function') }}">Function</a></li>
+        <li><a href="{{ url('/conditional') }}">Conditional</a></li>
+        <li><a href="{{ url('/looping') }}">Looping</a></li>
+        <li><a href="{{ url('/array') }}">Array</a></li>
+       </ol>
+    </body>
+</html>
